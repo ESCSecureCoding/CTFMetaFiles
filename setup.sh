33b7800734fe232e45b048da9336aa99467a2669
Ediz Turcan <0x6564697a@turcan.de>
2020-04-22 21:14:25 +0200

    sleep added

--- setup.sh
+++ setup.sh
@@ -53,4 +53,6 @@ docker network create http_proxy
 docker run --detach --name nginx-proxy --network http_proxy --publish 80:80 --publish 443:443 --volume /etc/nginx/certs --volume /etc/nginx/vhost.d --volume /usr/share/nginx/html --volume /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
 docker run --detach --name nginx-proxy-letsencrypt --network http_proxy --volumes-from nginx-proxy --volume /var/run/docker.sock:/var/run/docker.sock:ro --env "DEFAULT_EMAIL=ediz.turcan@exxeta.com" jrcs/letsencrypt-nginx-proxy-companion
 
+sleep 5m
+
 docker-compose -f /var/CTF/CTFd/docker-compose.yml up
